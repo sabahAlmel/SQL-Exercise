d67fad1a72dd99dcb2e972ede1ecdba25bde47c5
Sabah <sabahalmel7@gmail.com>
2023-09-16 18:35:20 +0300

    Joins

--- answers.sh
+++ answers.sh
@@ -20,3 +20,16 @@ Graduation text
 insert into graduates(Name,Age,Gender,Points,Graduation)
 values('Layal',18,"F",350,"2018-09-08")
 delete from students where Name='Layal'
+
+#### Joins
+select e.Name,e.Company,c.Date 
+from employees e,companies c
+where e.Company=c.Name;
+
+select e.Name 
+from employees e,companies c
+where c.Name=e.Company and c.date <2000
+
+select c.Name
+from employees e,companies c
+where e.Company=c.Name and e.Role="Graphic Designer"
