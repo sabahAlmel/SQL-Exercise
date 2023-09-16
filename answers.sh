b7296a6d36eb61c18d945a15a120ad7e0fc3d164
Sabah <sabahalmel7@gmail.com>
2023-09-16 18:04:39 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -7,3 +7,16 @@ select Points from students where Name="Alex"
 insert into students(ID,Name,Age,Gender,Points) values(7,"sabah","21","F",1000)
 update students set Points=400 where Name="Basma"
 update students set Points=100 where Name="Alex"
+
+#### Creating Table
+create table graduates(
+Id integer not null primary key autoincrement,
+Name text not null unique,
+Age integer ,
+Gender text ,
+Points integer,
+Graduation text
+)
+insert into graduates(Name,Age,Gender,Points,Graduation)
+values('Layal',18,"F",350,"2018-09-08")
+delete from students where Name='Layal'
